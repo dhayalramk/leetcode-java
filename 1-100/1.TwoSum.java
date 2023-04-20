682eabdeecfae01f863d8e921ecbfc4788afa646
Dhayal Ram <31826251+dhayalramk@users.noreply.github.com>
2023-04-20 15:52:54 +0000

    1. Two Sum

---
+++ 1-100/1.TwoSum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0;i<nums.length;i++){
+            int n = nums[i];
+            int diff = target - n;
+            if(map.containsKey(diff)) return new int[]{map.get(diff), i};
+            map.put(n, i);
+        }
+        return null;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # leetcode-java
+========
+### LeetCode Java Solutions
+
+| # | Title | Solution | Difficulty |
+|---| ----- | -------- | ---------- |
+|1|[Two Sum](https://leetcode.com/problems/two-sum/) | [Java](./1-100/1.TwoSum.java)|Easy|
